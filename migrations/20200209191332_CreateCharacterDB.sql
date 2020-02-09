1bf180952c20a40ebcd6fa7d34194afa0e5e8bf3
shinnosuke-K <shin.050195@gmail.com>
2020-02-09 19:59:11 +0900

    create migration sql

---
+++ migrations/20200209191332_CreateCharacterDB.sql
@@ -0,0 +1,14 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+create table if not exists characters (
+    chara_id varchar(20) not null primary key,
+    chara_name varchar(255),
+    reg_time datetime,
+    reg_time_jst datetime,
+    rare int
+);
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+drop table  characters;
