f296771d7c0949a758a98fbf57c59a834d54bb9b
shinnosuke-K <shin.050195@gmail.com>
2020-02-23 13:14:57 +0900

    change length of user_id

--- migrations/20200209191238_CreateUserDB.sql
+++ migrations/20200209191238_CreateUserDB.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 CREATE TABLE IF NOT EXISTS `users` (
-    user_id varchar(20) not null primary key,
+    user_id varchar(32) not null primary key,
     token varchar(255),
     user_name varchar(255),
     reg_time datetime,
