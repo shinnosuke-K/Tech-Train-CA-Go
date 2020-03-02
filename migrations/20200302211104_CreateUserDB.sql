1828f4b0a99f94e2bc981b8ca2cff74c0db61a82
shinnosuke-K <shin.050195@gmail.com>
2020-03-02 21:21:28 +0900

    fix column name
    カラムの名前が間違ってので変更
    それに伴って新しくマイグレーションファイルを作成

---
+++ migrations/20200302211104_CreateUserDB.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE IF NOT EXISTS `users` (
+    user_id varchar(32) not null primary key,
+    token varchar(255),
+    user_name varchar(255),
+    reg_time datetime,
+    reg_time_jst datetime,
+    update_time datetime,
+    update_time_jst datetime
+);
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+drop table users;
