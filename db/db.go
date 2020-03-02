69ca646d8a36714025800abdf64951614fa67dfb
shinnosuke-K <shin.050195@gmail.com>
2020-03-02 21:24:49 +0900

    create function
    データの存在確認の関数を追加

--- db/db.go
+++ db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -13,4 +14,23 @@ func Open() (*gorm.DB, error) {
 	connectTemplate := "%s:%s@/%s?%s"
 	connectPath := fmt.Sprintf(connectTemplate, os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_OPTION"))
 	return gorm.Open("mysql", connectPath)
+}
+
+type User struct {
+	UserId        string    `json:"user_id"`
+	Token         string    `json:"token"`
+	UserName      string    `json:"user_name"`
+	RegTime       time.Time `json:"reg_time"`
+	RegTimeJST    time.Time `json:"reg_time_jst"`
+	UpdateTime    time.Time `json:"update_time"`
+	UpdateTimeJST time.Time `json:"update_time_jst"`
+}
+
+func (userInfo *User) IsRecord(DB *gorm.DB) bool {
+	var user User
+	if err := DB.Where("user_id=?", userInfo.UserId).First(&user).Error; gorm.IsRecordNotFoundError(err) {
+		return true
+	} else {
+		return false
+	}
 }
