0c52cf14a683f2d149a17b0020857699664253ff
shinnosuke-K <shin.050195@gmail.com>
2020-03-09 16:21:57 +0900

    delete column
    トークンを保持するカラムを削除

--- migrations/20200302211104_CreateUserDB.sql
+++ migrations/20200302211104_CreateUserDB.sql
@@ -2,7 +2,6 @@
 -- SQL in section 'Up' is executed when this migration is applied
 CREATE TABLE IF NOT EXISTS `users` (
     user_id varchar(32) not null primary key,
-    token varchar(255),
     user_name varchar(255),
     reg_time_jst datetime,
     update_time_jst datetime
