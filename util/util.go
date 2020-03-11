c5f4f0152691b1bb6f7b61b2ce07cfc0811c7a98
shinnosuke-K <shin.050195@gmail.com>
2020-03-11 20:15:06 +0900

    divide
    どこにも属さないような関数の切り分け

---
+++ util/util.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+
+	"github.com/google/uuid"
+)
+
+func CreateUserId() string {
+	userId := uuid.Must(uuid.NewRandom())
+	return strings.ReplaceAll(userId.String(), "-", "")
+}
+
+func GetJSTTime() time.Time {
+	timeUTC := time.Now().UTC()
+	jst, _ := time.LoadLocation("Asia/Tokyo")
+	updateTimeJST := timeUTC.In(jst)
+	return updateTimeJST
+}
+
+func ParsedJWTToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("%s", "Unexpected signing method")
+
+		} else {
+			keyData, err := ioutil.ReadFile(os.Getenv("KEY_PATH"))
+			if err != nil {
+				return nil, err
+			}
+			return keyData, nil
+		}
+	})
+}
