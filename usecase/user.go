7cdb4360048ff56ea19baa24224ceeca44fc26ea
shinnosuke-K <shin.050195@gmail.com>
2021-01-20 16:22:06 +0900

    create: usecase層のの実装の雛形

---
+++ usecase/user.go
@@ -0,0 +1,42 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/shinnosuke-K/Tech-Train-CA-Go/domain/repository"
+
+	"github.com/shinnosuke-K/Tech-Train-CA-Go/domain/model"
+)
+
+type UserUseCase interface {
+	IsRecord(ctx context.Context, id int) bool
+	Add(ctx context.Context) error
+	Get(ctx context.Context, id int) (*model.User, error)
+	Update(ctx context.Context) error
+}
+
+type userUseCase struct {
+	userRepository repository.UserRepository
+}
+
+func NewUserUseCase(ur repository.UserRepository) UserUseCase {
+	return &userUseCase{
+		userRepository: ur,
+	}
+}
+
+func (u userUseCase) IsRecord(ctx context.Context, id int) bool {
+	panic("implement me")
+}
+
+func (u userUseCase) Add(ctx context.Context) error {
+	panic("implement me")
+}
+
+func (u userUseCase) Get(ctx context.Context, id int) (*model.User, error) {
+	panic("implement me")
+}
+
+func (u userUseCase) Update(ctx context.Context) error {
+	panic("implement me")
+}
