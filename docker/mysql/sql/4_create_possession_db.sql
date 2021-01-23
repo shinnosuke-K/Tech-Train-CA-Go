b8a14604dce24cc6c03d854a6d634cb0ea3d448a
shinnosuke-K <shin.050195@gmail.com>
2021-01-23 17:28:15 +0900

    add: 所持数のカラム

--- docker/mysql/sql/4_create_possession_db.sql
+++ docker/mysql/sql/4_create_possession_db.sql
@@ -2,6 +2,7 @@ create table if not exists tech_train_ca_go.possessions (
     posse_id varchar(36) not null,
     user_id varchar(36) not null,
     chara_id varchar(255) not null,
+    quantity int,
     reg_at datetime,
     update_at datetime,
     PRIMARY KEY (posse_id),
