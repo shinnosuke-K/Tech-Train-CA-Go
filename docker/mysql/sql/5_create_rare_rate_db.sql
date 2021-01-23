a0fd89ed64fba9fb889b434111b7ab8f13fafd72
shinnosuke-K <shin.050195@gmail.com>
2021-01-23 16:25:38 +0900

    add: データの挿入

--- docker/mysql/sql/5_create_rare_rate_db.sql
+++ docker/mysql/sql/5_create_rare_rate_db.sql
@@ -4,3 +4,10 @@ create table if not exists tech_train_ca_go.rare_rate (
     probability float,
     PRIMARY KEY (id)
 );
+
+insert into tech_train_ca_go.rare_rate(rarity, probability)
+values (5, 2.5),
+       (4, 7.5),
+       (3, 10),
+       (2, 20),
+       (1, 60)
