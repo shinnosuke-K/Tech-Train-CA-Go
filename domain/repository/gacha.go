b6acfe5a13a6721f17a48d322c294b6bfdc63a33
shinnosuke-K <shin.050195@gmail.com>
2021-01-24 21:30:49 +0900

    add: レコードの存在有無を確認する処理の雛形

--- domain/repository/gacha.go
+++ domain/repository/gacha.go
@@ -3,6 +3,7 @@ package repository
 import "github.com/shinnosuke-K/Tech-Train-CA-Go/domain/model"
 
 type GachaRepository interface {
+	IsRecord(table, id string) bool
 	GetRareRate() ([]*model.Gacha, error)
 	GetCharacter() ([]*model.Character, error)
 }
