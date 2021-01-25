4394ffde9348f3e12ddf51202d671fc0ca07a014
shinnosuke-K <shin.050195@gmail.com>
2021-01-25 13:06:01 +0900

    add: 仮のガチャ機能

--- usecase/gacha.go
+++ usecase/gacha.go
@@ -1,16 +1,20 @@
 package usecase
 
-import "github.com/shinnosuke-K/Tech-Train-CA-Go/domain/repository"
+import (
+	"math/rand"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/shinnosuke-K/Tech-Train-CA-Go/domain/repository"
+)
 
 type Result struct {
-	Results []struct {
-		CharaId int
-		Name    string
-	}
+	CharaId int
+	Name    string
 }
 
 type GachaUseCase interface {
-	Draw(times int) (Result, error)
+	Draw(times int) ([]*Result, error)
 }
 
 type gachaUseCase struct {
@@ -23,6 +27,40 @@ func NewGachaUseCase(ug repository.GachaRepository) GachaUseCase {
 	}
 }
 
-func (g gachaUseCase) Draw(times int) (Result, error) {
-	panic("implement me")
+func (g gachaUseCase) Draw(times int) ([]*Result, error) {
+
+	gacha, err := g.gachaRepository.GetRareRate()
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't get the Gacha record")
+	}
+
+	chara, err := g.gachaRepository.GetCharacter()
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't get the Character record")
+	}
+
+	rand.Seed(time.Now().Unix())
+
+	r := make([]*Result, 0, times)
+	for n := 0; n < times; n++ {
+		p := rand.Float64() * 100
+		total := 0.0
+		for _, c := range chara {
+			for _, g := range gacha {
+				if g.Rarity == c.Rarity {
+					total += g.Probability / float64(len(chara))
+					break
+				}
+			}
+			if total >= p {
+				r = append(r, &Result{
+					CharaId: c.CharaId,
+					Name:    c.CharaName,
+				})
+				break
+			}
+		}
+	}
+
+	return r, nil
 }
