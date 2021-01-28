d25e7ef08f8a11f0af52483ea191f144163ce644
shinnosuke-K <shin.050195@gmail.com>
2021-01-28 20:42:41 +0900

    fix: 構造体の変数名

--- domain/model/possession.go
+++ domain/model/possession.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Possession struct {
-	PosseId string
-	UserId  string
-	CharaId string
-	RegAt   time.Time
+	Id      string    `gorm:"type:varchar(36);column:id;primary_key"`
+	UserId  string    `gorm:"type:varchar(36);column:user_id;foreign_key"`
+	CharaId string    `gorm:"type:varchar(255);column:chara_id"`
+	RegAt   time.Time `gorm:"type:datetime;column:reg_at"`
 }
