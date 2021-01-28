b3ed1cbe527f037f75a96ad5d5060f30cddeff0c
shinnosuke-K <shin.050195@gmail.com>
2021-01-28 16:33:45 +0900

    change: データ構造変更に伴い値と型を変更

--- domain/model/character.go
+++ domain/model/character.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 type Character struct {
-	CharaId   int       `gorm:"type:int;column:chara_id;primary_key"`
+	CharaId   string    `gorm:"type:int;column:chara_id;primary_key"`
 	CharaName string    `gorm:"type:varchar(255);column:chara_name"`
 	RegAt     time.Time `gorm:"type:datetime;column:reg_at"`
 	Rarity    int       `gorm:"type:int;column:rarity"`
