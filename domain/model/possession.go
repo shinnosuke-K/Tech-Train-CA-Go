0a253165fb62671b7da63b2bc3a4ecf9efc1f4af
shinnosuke-K <shin.050195@gmail.com>
2021-01-28 16:33:45 +0900

    change: データ構造変更に伴い値と型を変更

--- domain/model/possession.go
+++ domain/model/possession.go
@@ -3,9 +3,8 @@ package model
 import "time"
 
 type Possession struct {
-	PosseId  string    `gorm:"type:varchar(36);column:posse_id;primary_key"`
-	UserId   string    `gorm:"type:varchar(36);column:posse_id;foreign_key"`
-	CharaId  string    `gorm:"type:varchar(255);column:chara_id"`
-	RegAt    time.Time `gorm:"type:datetime;column:reg_at"`
-	UpdateAt time.Time `gorm:"type:datetime;column:update_at"`
+	PosseId string    `gorm:"type:varchar(36);column:posse_id;primary_key"`
+	UserId  string    `gorm:"type:varchar(36);column:posse_id;foreign_key"`
+	CharaId string    `gorm:"type:varchar(255);column:chara_id"`
+	RegAt   time.Time `gorm:"type:datetime;column:reg_at"`
 }
