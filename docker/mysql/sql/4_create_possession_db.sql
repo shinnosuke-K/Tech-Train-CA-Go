a75a0c8b9ed61435e1819340210d5d31ac90a7ad
shinnosuke-K <shin.050195@gmail.com>
2021-01-28 20:42:08 +0900

    change: カラム名

--- docker/mysql/sql/4_create_possession_db.sql
+++ docker/mysql/sql/4_create_possession_db.sql
@@ -1,9 +1,9 @@
 create table if not exists tech_train_ca_go.possessions (
-    posse_id varchar(36) not null,
+    id varchar(36) not null,
     user_id varchar(36) not null,
     chara_id varchar(255) not null,
     reg_at datetime,
-    PRIMARY KEY (posse_id),
+    PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES tech_train_ca_go.users(user_id)
 );
 
