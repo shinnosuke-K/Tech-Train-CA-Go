2617489b443e5b640195a9da9a3707670ea3c003
shinnosuke-K <shin.050195@gmail.com>
2021-01-28 17:34:02 +0900

    change: テーブル名

--- docker/mysql/sql/5_create_rare_rate_db.sql
+++ docker/mysql/sql/5_create_rare_rate_db.sql
@@ -1,11 +1,11 @@
-create table if not exists tech_train_ca_go.rare_rate (
+create table if not exists tech_train_ca_go.gachas (
     id int not null auto_increment,
     rarity int,
     probability float,
     PRIMARY KEY (id)
 );
 
-insert into tech_train_ca_go.rare_rate(rarity, probability)
+insert into tech_train_ca_go.gachas(rarity, probability)
 values (5, 2.5),
        (4, 7.5),
        (3, 10),
