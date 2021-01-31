b8e7720339bc867e90bb585e6cdcb870bab1d143
shinnosuke-K <shin.050195@gmail.com>
2021-01-31 13:48:34 +0900

    fix: カラム名

--- domain/model/character.go
+++ domain/model/character.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 type Character struct {
-	Id     string    `gorm:"type:int;column:chara_id;primary_key"`
+	Id     string    `gorm:"type:int;column:id;primary_key"`
 	Name   string    `gorm:"type:varchar(255);column:chara_name"`
 	RegAt  time.Time `gorm:"type:datetime;column:reg_at"`
 	Rarity int       `gorm:"type:int;column:rarity"`
