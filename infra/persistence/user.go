9d52bd7061d18c13828d83e1df604510ba41975b
shinnosuke-K <shin.050195@gmail.com>
2021-02-08 14:37:13 +0900

    add: エラーを追いやすいようにスタックを追加

--- infra/persistence/user.go
+++ infra/persistence/user.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/pkg/errors"
+
 	"github.com/shinnosuke-K/Tech-Train-CA-Go/domain/model"
 	"github.com/shinnosuke-K/Tech-Train-CA-Go/domain/repository"
 )
@@ -30,17 +32,17 @@ func (u userPersistence) IsRecord(id string) bool {
 func (u userPersistence) Add(user *model.User) error {
 	tx, err := u.DB.Begin()
 	if err != nil {
-		return err
+		return errors.WithStack(err)
 	}
 
 	_, err = tx.Query("insert into users(user_id, user_name, reg_at, update_at) values (?, ?, ?, ?)", user.ID, user.Name, user.RegAt, user.UpdateAt)
 	if err != nil {
 		tx.Rollback()
-		return err
+		return errors.WithStack(err)
 	}
 
 	if err := tx.Commit(); err != nil {
-		return err
+		return errors.WithStack(err)
 	}
 
 	return nil
@@ -49,13 +51,13 @@ func (u userPersistence) Add(user *model.User) error {
 func (u userPersistence) Get(id string) (*model.User, error) {
 	rows, err := u.DB.Query("select * from users where user_id = ?", id)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(err)
 	}
 
 	var user model.User
 	for rows.Next() {
 		if err := rows.Scan(&user.ID, &user.Name, &user.RegAt, &user.UpdateAt); err != nil {
-			return nil, err
+			return nil, errors.WithStack(err)
 		}
 	}
 
@@ -65,17 +67,17 @@ func (u userPersistence) Get(id string) (*model.User, error) {
 func (u userPersistence) Update(user *model.User) error {
 	tx, err := u.DB.Begin()
 	if err != nil {
-		return err
+		return errors.WithStack(err)
 	}
 
 	_, err = tx.Query("update users set user_name=?, update_at=? where user_id=?", user.Name, user.UpdateAt, user.ID)
 	if err != nil {
 		tx.Rollback()
-		return err
+		return errors.WithStack(err)
 	}
 
 	if err := tx.Commit(); err != nil {
-		return err
+		return errors.WithStack(err)
 	}
 
 	return nil
