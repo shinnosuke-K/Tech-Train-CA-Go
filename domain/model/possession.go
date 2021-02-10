21c137961348a4d2ff922047aa4636ec5cc99c33
shinnosuke-K <shin.050195@gmail.com>
2021-02-10 20:32:36 +0900

    change: 変数名とタグを追加

--- domain/model/possession.go
+++ domain/model/possession.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Possession struct {
-	Id      string    `db:"id"`
-	UserId  string    `db:"user_id"`
-	CharaId string    `db:"chara_id"`
+	ID      string    `db:"id"`
+	UserID  string    `db:"user_id"`
+	CharaID string    `db:"chara_id"`
 	RegAt   time.Time `db:"reg_at"`
 }
--- domain/model/user.go
+++ domain/model/user.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type User struct {
-	ID       string
-	Name     string
-	RegAt    time.Time
-	UpdateAt time.Time
+	ID       string    `db:"id"`
+	Name     string    `db:"name"`
+	RegAt    time.Time `db:"reg_at"`
+	UpdateAt time.Time `db:"update_at"`
 }
