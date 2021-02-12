0c6dadd3112af1e7a88d39bb3db04c5e6aa125bd
shinnosuke-K <shin.050195@gmail.com>
2021-02-12 11:49:03 +0900

    fix: カラム名

--- infra/persistence/user.go
+++ infra/persistence/user.go
@@ -21,7 +21,7 @@ func NewUserPersistence(db *sql.DB) repository.UserRepository {
 }
 
 func (u userPersistence) IsRecord(id string) bool {
-	_, err := u.DB.Query("select * from users where user_id = ?", id)
+	_, err := u.DB.Query("select * from users where id = ?", id)
 	if err != nil {
 		log.Println(err)
 		return false
@@ -35,7 +35,7 @@ func (u userPersistence) Add(user *model.User) error {
 		return errors.WithStack(err)
 	}
 
-	_, err = tx.Exec("insert into users(user_id, user_name, reg_at, update_at) values (?, ?, ?, ?)", user.ID, user.Name, user.RegAt, user.UpdateAt)
+	_, err = tx.Exec("insert into users(id, name, reg_at, update_at) values (?, ?, ?, ?)", user.ID, user.Name, user.RegAt, user.UpdateAt)
 	if err != nil {
 		tx.Rollback()
 		return errors.WithStack(err)
@@ -49,7 +49,7 @@ func (u userPersistence) Add(user *model.User) error {
 }
 
 func (u userPersistence) Get(id string) (*model.User, error) {
-	rows, err := u.DB.Query("select * from users where user_id = ?", id)
+	rows, err := u.DB.Query("select * from users where id = ?", id)
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
@@ -70,7 +70,7 @@ func (u userPersistence) Update(user *model.User) error {
 		return errors.WithStack(err)
 	}
 
-	_, err = tx.Exec("update users set user_name=?, update_at=? where user_id=?", user.Name, user.UpdateAt, user.ID)
+	_, err = tx.Exec("update users set name=?, update_at=? where id=?", user.Name, user.UpdateAt, user.ID)
 	if err != nil {
 		tx.Rollback()
 		return errors.WithStack(err)
